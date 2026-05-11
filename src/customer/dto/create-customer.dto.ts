b4b8dd803a0cd39cc9a6fde4a7611063472a3b76
Vishal Mishra <vishalmishra4400@gmail.com>
2026-05-11 07:31:07 +0530

    class-validator class transformer

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,4 +1,8 @@
+import { IsString } from "class-validator";
+
 export class customerDto{
+    @IsString()// from the package class validator class transformer
     name!:string;
+    @IsString()
     age!:number
 }
